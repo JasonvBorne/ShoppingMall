437540e778375f72984d53f141b0bc532954868d
相博珩 <421080367@qq.com>
2019-05-08 11:37:07 +0800

    jdbc配置

--- src/main/java/com/example/demo/control/HelloController.java
+++ src/main/java/com/example/demo/control/HelloController.java
@@ -1,15 +1,21 @@
 package com.example.demo.control;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.sql.DataSource;
+
 @Controller
 public class HelloController {
+    @Autowired
+    private DataSource dataSource;
+
     @RequestMapping("/")
     @ResponseBody
     public String index(){
-        return "Hello Springboot";
+        return "Hello Springboot"+dataSource;
     }
 }
